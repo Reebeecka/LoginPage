c391cfe5cdf6c2fe02a67c3b3887810e844fe391
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-05-23 17:13:03 +0200

    G done

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -23,50 +23,74 @@ router.get('/', (req, res) => {
     <button type="submit">Logga in</button>
     </div>
 </form>`
-res.send(form);
+    res.send(form);
 
-  });
+});
 
-  router.post("/login", function (req, res) {
+router.post("/login", function (req, res) {
 
-    if(req.body.name == "Admin" && req.body.password =="Admin"){
+    if (req.body.name == "Admin" && req.body.password == "Admin") {
         login = true;
         res.redirect("/admin/logedin");
     }
-    else{
+    else {
         res.send("Fel användarnamn eller lösenord");
     }
-        });
+});
 
 
-  router.get('/logedin', async (req, res) => {
-      if (login){
+router.get('/logedin', async (req, res) => {
+    if (login) {
         const users = await userModel.find();
         let logout = '<a href="/admin">Logga ut</a>'
+        let emailList = '<a href="/admin/emailList">Se e-post lista för nyhetsbrev</a>'
         var theUsers = "<div>";
-    
-        for(var i = 0; i< users.length; i++){
+
+        for (var i = 0; i < users.length; i++) {
 
             let text = "";
-            if(!users[i].newsletter){
+            if (!users[i].newsletter) {
                 text = "inte "
             }
-            else {text = ""};
-            
-             let user = `<div>
-             <h1>${users[i].username}</h1>
-             <h2>${users[i].email}</h2>
-             <h3>Prenumererar ` + text + `på brevet</h3>
+            else { text = "" };
+
+            let user = `<div>
+             <h3>${users[i].username}</h3>
+             <p>${users[i].email}</p>
+             <p>Prenumererar ` + text + `på brevet</p>
              `
-             theUsers += user;
-         }
-         theUsers += '</div>';
-    
-         res.send(logout + theUsers);
-      }
-      else{
-          res.redirect("/admin");
-      }
-  })
-  
-  module.exports = router;
+            theUsers += user;
+        }
+        theUsers += '</div>';
+
+        res.send(logout +"</br>"+ emailList + theUsers);
+    }
+    else {
+        res.redirect("/admin");
+    }
+});
+
+router.get('/emailList', async (req, res) => {
+    if (login) {
+        const users = await userModel.find();
+        let logout = '<a href="/admin">Logga ut</a>'
+        let emailList = '<a href="/admin/logedin">Till alla användare</a>'
+        var theUsers = "<ul>";
+
+        for (var i = 0; i < users.length; i++) {
+
+            let user;
+            if (!users[i].newsletter) {user=""}
+            else { user =  `<li>${users[i].email}</li>`};
+
+            theUsers += user;
+        }
+        theUsers += '</ul>';
+
+        res.send(logout +"</br>"+ emailList + theUsers);
+    }
+    else {
+        res.redirect("/admin");
+    }
+})
+module.exports = router;
